467c5fd8609242ff01d855abcd63d42b97e9a0b6
hbeanjarah <hbeanjarahoussen@gmail.com>
2026-07-31 06:38:45 +0300

    build(server): set up local node + typescript environment

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ import prettier from 'eslint-config-prettier';
 import globals from 'globals';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['**/dist'] },
   js.configs.recommended,
   ...tseslint.configs.recommended,
   reactHooks.configs.flat['recommended-latest'],
